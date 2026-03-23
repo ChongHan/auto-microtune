48603e24b8bec98d4a7d761a1d012176ac82f1df
Chong Han <chong@xiaohanc.com>
2026-03-23 23:17:47 +0100

    Revert "tock: lower the price-level map load factor to 0.3"
    This reverts commit 5a67d7a91d48b0d3a8dffece282c29046c8d745c.

--- src/main/java/com/xiaohanc/orderbook/OrderBookImpl.java
+++ src/main/java/com/xiaohanc/orderbook/OrderBookImpl.java
@@ -248,7 +248,7 @@ public class OrderBookImpl implements OrderBook {
 
         private final boolean buySide;
         private final byte sideFlag;
-        private final LongIntMap levels = new LongIntMap(256, 0.3f, NO_INDEX);
+        private final LongIntMap levels = new LongIntMap(256, 0.35f, NO_INDEX);
 
         private long[] levelPrices = new long[INITIAL_LEVEL_CAPACITY];
         private long[] levelKeys = new long[INITIAL_LEVEL_CAPACITY];
